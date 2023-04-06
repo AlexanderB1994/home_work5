9aa0747e406ad26d98e9ec4586ac4bbe17820e13
AlexanderB1994 <bulgaru.alexander1994@gmail.com>
2023-04-06 14:52:22 +0300

    update2

--- scrypt.py
+++ scrypt.py
@@ -1,37 +1,61 @@
 import csv
 import json
 import os
+import shutil
 import uuid
+from itertools import groupby
 
+
+def is_slow_car(hp):
+    return hp < 120
+
+
+def is_fast_car(hp):
+    return 120 <= hp < 180
+
+
+def is_sport_car(hp):
+    return hp >= 180
+
+
+def is_cheap_car(price):
+    return price < 20000
+
+
+def is_medium_car(price):
+    return 20000 <= price < 50000
+
+
+def is_expensive_car(price):
+    return price >= 50000
+
+
+def write_json_file(file_path, data):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+categories = {
+    'slow_cars': is_slow_car,
+    'fast_cars': is_fast_car,
+    'sport_cars': is_sport_car,
+    'cheap_cars': is_cheap_car,
+    'medium_cars': is_medium_car,
+    'expensive_cars': is_expensive_car,
+}
 
 if __name__ == '__main__':
     input_file = 'input.csv'
-    vehicles = []
-    with open(input_file) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            row['id'] = str(uuid.uuid4())
-            row['hp'] = int(row['hp'])
-            row['price'] = float(row['price'])
-            vehicles.append(row)
-
-    categories = {
-        'slow_cars': lambda hp: hp < 120,
-        'fast_cars': lambda hp: 120 <= hp < 180,
-        'sport_cars': lambda hp: hp >= 180,
-        'cheap_cars': lambda price: price < 20000,
-        'medium_cars': lambda price: 20000 <= price < 50000,
-        'expensive_cars': lambda price: price >= 50000,
-    }
+    vehicles = [
+        {**row, 'id': str(uuid.uuid4()), 'hp': int(row['hp']), 'price': float(row['price'])}
+        for row in csv.DictReader(open(input_file))
+    ]
 
     output_dir = 'output_data'
     if os.path.exists(output_dir):
-        for category in categories:
-            category_file = os.path.join(output_dir, category + '.json')
-            if os.path.exists(category_file):
-                os.remove(category_file)
-    else:
-        os.mkdir(output_dir)
+        shutil.rmtree(output_dir)
+
+    os.makedirs(output_dir)
 
     for category, condition in categories.items():
         category_file = os.path.join(output_dir, category + '.json')
@@ -39,12 +63,10 @@ if __name__ == '__main__':
             category_data = [vehicle for vehicle in vehicles if condition(vehicle['price'])]
         else:
             category_data = [vehicle for vehicle in vehicles if condition(vehicle['hp'])]
-        with open(category_file, 'w') as f:
-            json.dump(category_data, f, indent=2)
+        write_json_file(category_file, category_data)
 
-    brands = set(vehicle['brand'] for vehicle in vehicles)
-    for brand in brands:
+    vehicles.sort(key=lambda vehicle: vehicle['brand'])
+    for brand, group in groupby(vehicles, key=lambda vehicle: vehicle['brand']):
         brand_file = os.path.join(output_dir, brand.lower() + '.json')
-        brand_data = [vehicle for vehicle in vehicles if vehicle['brand'].lower() == brand.lower()]
-        with open(brand_file, 'w') as f:
-            json.dump(brand_data, f, indent=2)
+        brand_data = list(group)
+        write_json_file(brand_file, brand_data)
